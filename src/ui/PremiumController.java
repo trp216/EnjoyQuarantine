4c2fd92dc382200ee0530ae4fbf209baba5ebb6b
Alejandra Diaz <alejandradp19@gmail.com>
2020-05-02 19:18:23 -0500

    Actualized PremiumController and connected it with AccountController

--- src/ui/PremiumController.java
+++ src/ui/PremiumController.java
@@ -1,13 +1,75 @@
 package ui;
 
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.MenuItem;
+import javafx.stage.Stage;
 
 public class PremiumController {
 	
 	private Scene  scene;
+	
+	private AccountController ac;
 
+	@FXML
+    private MenuItem friends;
+
+    @FXML
+    private MenuItem seeActivity;
+
+    @FXML
+    private MenuItem addActivity;
+
+    @FXML
+    private MenuItem motquote;
+
+    @FXML
+    private MenuItem health;
+    
 	public void setScene(Scene scene) {
 		this.scene = scene;
 	}
 
+    @FXML
+    void addActivities(ActionEvent event) {
+
+    }
+
+    @FXML
+    void getHealthTip(ActionEvent event) {
+
+    }
+
+    @FXML
+    void getMotivationalQuote(ActionEvent event) {
+
+    }
+
+    @FXML
+    void seeActivities(ActionEvent event) {
+
+    }
+
+    @FXML
+    void seeOtherAccounts(ActionEvent event) {
+    	 try {
+  			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Accounts.fxml"));
+  			Parent root = fxmlLoader.load();
+  			Stage stage = new Stage();
+  			Scene scene = new Scene(root);
+  			ac = fxmlLoader.getController();
+  			ac.setScene(scene);
+  			stage.setScene(scene);
+  			stage.show();
+      }
+      catch(IOException io) {
+      	io.printStackTrace();
+      }
+    }
 }
