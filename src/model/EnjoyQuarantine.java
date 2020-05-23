9aa6a03ef08604b761f9df595deb7928616ad671
Alejandra Diaz <alejandradp19@gmail.com>
2020-05-23 18:49:41 -0500

    Actualized class diagram. Added button and methods to show the information of the accounts on AccountController. Added methods to sort the accounts in EnjoyQuarantine

--- src/model/EnjoyQuarantine.java
+++ src/model/EnjoyQuarantine.java
@@ -13,6 +13,44 @@ public class EnjoyQuarantine {
 	public EnjoyQuarantine() {
 		accounts = new ArrayList<Account>();
 	}
+	
+	
+
+	public ArrayList<Account> getAccounts() {
+		return accounts;
+	}
+
+
+
+	public void setAccounts(ArrayList<Account> accounts) {
+		this.accounts = accounts;
+	}
+
+
+
+	public Activity getActivities() {
+		return activities;
+	}
+
+
+
+	public void setActivities(Activity activities) {
+		this.activities = activities;
+	}
+
+
+
+	public MotivationalQuote getRootmq() {
+		return rootmq;
+	}
+
+
+
+	public void setRootmq(MotivationalQuote rootmq) {
+		this.rootmq = rootmq;
+	}
+
+
 
 	public void addAccount(String username, String name, String password, String birthdate, String gender, double height,
 			double weight, boolean isPremium) {
@@ -67,4 +105,80 @@ public class EnjoyQuarantine {
 		             }
 		        }
 		    }
+		
+	public void bubbleSortN() {
+		for(int i = accounts.size();i>0;i--) {
+			for(int j=0;j<i-1;j++) {
+				if(accounts.get(j).getName().compareTo(accounts.get(j+1).getName())>0) {
+					Account temp = accounts.get(j);
+					accounts.set(j, accounts.get(j+1));
+					accounts.set(j+1, temp);
+				}
+			}
+		}
+	}
+	
+	public void bubbleSortH() {
+		for(int i = accounts.size();i>0;i--) {
+			for(int j=0;j<i-1;j++) {
+				if(accounts.get(j).getHeight()>accounts.get(j+1).getHeight()) {
+					Account temp = accounts.get(j);
+					accounts.set(j, accounts.get(j+1));
+					accounts.set(j+1, temp);
+				}
+			}
+		}
+	}
+	
+	public void selectionSortU() {
+		for(int i = 0; i<accounts.size();i++) {
+			Account minor = getAccounts().get(i);
+			int which = i;
+			for(int j = i+1;j<accounts.size();j++) {
+				if(accounts.get(j).getUsername().compareTo(minor.getUsername())<0) {
+					minor = accounts.get(j);
+					which = j;
+				}
+			}
+			Account temp = accounts.get(i);
+			accounts.set(i, minor);
+			accounts.set(which, temp);
+		}
+	}
+	
+	public void selectionSortW() {
+		for(int i = 0; i<accounts.size();i++) {
+			Account minor = getAccounts().get(i);
+			int which = i;
+			for(int j = i+1;j<accounts.size();j++) {
+				if(accounts.get(j).getWeight()<minor.getWeight()) {
+					minor = accounts.get(j);
+					which = j;
+				}
+			}
+			Account temp = accounts.get(i);
+			accounts.set(i, minor);
+			accounts.set(which, temp);
+		}
+	}
+	
+	public void insertionSortH() {
+		for(int i = 1;i<accounts.size();i++) {
+			for(int j = i;j > 0 && accounts.get(j-1).getHeight()>accounts.get(j).getHeight();j--) {
+				Account temp = accounts.get(i);
+				accounts.set(j, accounts.get(j-1));
+				accounts.set(j-1, temp);
+			}
+		}
+	}
+	
+	public void insertionSortU() {
+		for(int i = 1;i<accounts.size();i++) {
+			for(int j = i;j > 0 && accounts.get(j-1).getUsername().compareTo(accounts.get(j).getUsername())>0;j--) {
+				Account temp = accounts.get(i);
+				accounts.set(j, accounts.get(j-1));
+				accounts.set(j-1, temp);
+			}
+		}
+	}
 }
--- src/ui/AccountController.java
+++ src/ui/AccountController.java
@@ -1,13 +1,127 @@
 package ui;
 
 import javafx.scene.Scene;
+import exceptions.MissingFieldsException;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.cell.PropertyValueFactory;
+import model.Account;
+import model.EnjoyQuarantine;
 
 public class AccountController {
+
+    @FXML
+    private TableView<Account> table;
+
+    @FXML
+    private TableColumn<Account, String> column1;
+
+    @FXML
+    private TableColumn<Account, String> column2;
+
+    @FXML
+    private TableColumn<Account, String> column3;
+
+    @FXML
+    private TableColumn<Account, String> column4;
+
+    @FXML
+    private TableColumn<Account, String> column5;
+
+    @FXML
+    private TableColumn<Account, Double> column6;
+
+    @FXML
+    private TableColumn<Account, Double> column7;
+
+    @FXML
+    private ChoiceBox<String> choicebox;
+    
+    @FXML
+    private Button go;
 	
 	private Scene scene;
+	
+	private EnjoyQuarantine eq;
 
 	public void setScene(Scene scene) {
 		this.scene = scene;
 	}
 
+	public void getEQ(EnjoyQuarantine eq) {
+		this.eq = eq;
+	}
+	
+	 private void initializeTableView() {
+		    
+	    	ObservableList<Account> observableList = FXCollections.observableArrayList(eq.getAccounts());
+	    	table.setItems(observableList);
+	    	
+	    	column1.setCellValueFactory(new PropertyValueFactory<Account,String>("username")); 
+	    	column2.setCellValueFactory(new PropertyValueFactory<Account,String>("name"));
+	    	column3.setCellValueFactory(new PropertyValueFactory<Account,String>("password"));
+	    	column4.setCellValueFactory(new PropertyValueFactory<Account,String>("birthdate"));
+	     	column5.setCellValueFactory(new PropertyValueFactory<Account,String>("gender"));
+	     	column6.setCellValueFactory(new PropertyValueFactory<Account,Double>("height"));
+	     	column7.setCellValueFactory(new PropertyValueFactory<Account,Double>("weight"));
+	    }
+	
+	@FXML
+	public void go(ActionEvent event) {
+		try {
+			if(choicebox.getValue().equals("")) {
+				throw new MissingFieldsException();
+			}
+			else {
+				if(choicebox.getValue().equals("Bubble Sort: Name")) {
+					eq.bubbleSortN();
+				}
+				else if(choicebox.getValue().equals("Bubble Sort: Height")) {
+					eq.bubbleSortH();
+				}
+				else if(choicebox.getValue().equals("Selection Sort: Username")) {
+					eq.selectionSortU();
+				}
+				else if(choicebox.getValue().equals("Selection Sort: Weight")) {
+					eq.selectionSortW();
+				}
+				else if(choicebox.getValue().equals("Insertion Sort: Height")) {
+					eq.insertionSortH();
+				}
+				else if(choicebox.getValue().equals("Insertion Sort: Username")) {
+					eq.selectionSortU();
+				}
+				
+				initializeTableView();
+			}
+		}
+		catch(MissingFieldsException m) {
+			Alert alert = new Alert(AlertType.INFORMATION);
+		    alert.setTitle("Warning filling the registration");
+		    alert.setHeaderText("Some fields are empty!");
+		    alert.setContentText("This is a friendly reminder for you to fill all fields :D");
+		
+		    alert.showAndWait();
+		}
+	}
+	
+    @FXML
+    void initialize() {
+    	choicebox.getItems().add("Bubble Sort: Name");
+    	choicebox.getItems().add("Bubble Sort: Height");
+    	choicebox.getItems().add("Selection Sort: Username");
+    	choicebox.getItems().add("Selection Sort: Weight");
+    	choicebox.getItems().add("Insertion Sort: Height");
+    	choicebox.getItems().add("Insertion Sort: Username");
+    	
+    	initializeTableView();
+    }
 }
